aa19474e6812bdeed5080c91412893f5074fe6c8
Jpanichphol <jpanichphol@gmail.com>
2024-03-26 14:00:26 +0700

    ✅ add coverage

--- src/merge.ts
+++ src/merge.ts
@@ -1,6 +1,22 @@
-export function merge (arr1: number[], arr2: number[], arr3: number[]): number[] {
-  const mergedList: number[] = arr1.concat(arr2).concat(arr3)
-  
+export function merge(
+  arr1: number[],
+  arr2: number[],
+  arr3: number[]
+): number[] {
+  const mergedList: number[] = arr1.concat(arr2).concat(arr3);
+
+  var done = false;
+  while (!done) {
+    done = true;
+    for (var i = 1; i < mergedList.length; i += 1) {
+      if (mergedList[i - 1] > mergedList[i]) {
+        done = false;
+        var tmp = mergedList[i - 1];
+        mergedList[i - 1] = mergedList[i];
+        mergedList[i] = tmp;
+      }
+    }
+  }
 
   return mergedList;
 }
--- tests/merge.test.ts
+++ tests/merge.test.ts
@@ -1,38 +1,38 @@
 import { merge } from "../src/merge";
 
 describe("merge function", () => {
-  it("should merge three sorted collections correctly", () => {
+  it("should merge sorted collections", () => {
     const collection1 = [1, 5, 7];
-    const collection2 = [6, 4, 2];
+    const collection2 = [6, 4, 0]; // sort max to min(0)
     const collection3 = [3, 8, 9];
 
-    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const expected = [0, 1, 3, 4, 5, 6, 7, 8, 9];
 
-    console.log(merge(collection1, collection2, collection3));
     expect(merge(collection1, collection2, collection3)).toEqual(
-      expectedResult
+      expected
     );
   });
 
-  // it('should handle collections with duplicates', () => {
-  //   const collection1 : number[] = [1, 1, 3];
-  //   const collection2 : number[] = [];
-  //   const collection3 : number[] = [2, 2, 8];
-  //   const expectedResult : number[] = [-2, 0, 0, 1, 1, 2, 2, 3, 8];
+  it("should handle duplicate", () => {
+    const collection1: number[] = [1, 1, 3];
+    const collection2: number[] = [6, 4, 4];
+    const collection3: number[] = [2, 2, 8];
+    const expected: number[] = [1, 1, 2, 2, 3, 4, 4, 6, 8];
 
-  //   console.log(merge(collection1, collection2, collection3))
+    expect(merge(collection1, collection2, collection3)).toEqual(
+      expected
+    );
+  });
 
-  //   expect(merge(collection1, collection2, collection3)).toEqual(expectedResult);
-  // });
-
-  it("should handle empty collections", () => {
+  it("should handle empty collections ", () => {
     const collection1: number[] = [];
     const collection2: number[] = [2];
     const collection3: number[] = [8];
-    const expectedResult: number[] = [2, 8];
-
+    const expected: number[] = [2, 8];
+    
     expect(merge(collection1, collection2, collection3)).toEqual(
-      expectedResult
+      expected
     );
   });
+
 });
